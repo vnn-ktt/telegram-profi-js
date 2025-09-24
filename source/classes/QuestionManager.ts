3a463aaf52ada112765ddcfa2b3a17bcb792d0fa
Ivan Platunov <i@iplatunov.ru>
2025-09-24 12:28:06 +0300

    профиджей->pretties_eslint 1. создан конфиг, выполнен рефактор

--- source/classes/QuestionManager.ts
+++ source/classes/QuestionManager.ts
@@ -1,68 +1,71 @@
-import {Random} from "random-js";
+import { Random } from "random-js";
 import {
-    EGrade,
-    EQuestionType,
-    IQuestionAnswer,
-    IQuestionClick,
-    TQuestionsByGrade,
-    TQuestion
+  EGrade,
+  EQuestionType,
+  IQuestionAnswer,
+  IQuestionClick,
+  TQuestionsByGrade,
+  TQuestion,
 } from "../types/questions.js";
-import data from '../database/questions.js';
+import data from "../database/questions.js";
 
 export default class QuestionManager {
-    private readonly _data: TQuestionsByGrade;
-    private readonly _random: Random;
+  private readonly _data: TQuestionsByGrade;
+  private readonly _random: Random;
 
-    constructor (){
-        this._data = data;
-        this._random = new Random();
-    }
+  constructor() {
+    this._data = data;
+    this._random = new Random();
+  }
 
-    public getRandomQuestion(
-        grade: string
-    ): IQuestionAnswer | IQuestionClick {
-        const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
-        if (!this._data[gradeKey]) {
-            throw new Error(`❌ No questions found for grade: ${grade}`);
-        }
-        const randomQuestionIndex = this._random.integer(
-            0,
-            data[gradeKey].length - 1
-        );
-        return data[gradeKey][randomQuestionIndex];
+  public getRandomQuestion(grade: string): IQuestionAnswer | IQuestionClick {
+    const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
+    if (!this._data[gradeKey]) {
+      throw new Error(`❌ No questions found for grade: ${grade}`);
     }
+    const randomQuestionIndex = this._random.integer(
+      0,
+      data[gradeKey].length - 1
+    );
+    return data[gradeKey][randomQuestionIndex];
+  }
 
-    public isClickQuestionType(
-        question: IQuestionAnswer | IQuestionClick
-    ) : question is IQuestionClick {
-        return question.type === EQuestionType.CLICK;
-    }
+  public isClickQuestionType(
+    question: IQuestionAnswer | IQuestionClick
+  ): question is IQuestionClick {
+    return question.type === EQuestionType.CLICK;
+  }
 
-    public isAnswerQuestionType(
-        question: IQuestionAnswer | IQuestionClick
-    ) : question is IQuestionAnswer  {
-        return question.type === EQuestionType.ANSWER;
-    }
+  public isAnswerQuestionType(
+    question: IQuestionAnswer | IQuestionClick
+  ): question is IQuestionAnswer {
+    return question.type === EQuestionType.ANSWER;
+  }
 
-    public getQuestionsCountByGrade(grade: string = EGrade.JUNIOR) : number {
-        const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
-        return this._data[gradeKey]?.length;
-    }
+  public getQuestionsCountByGrade(grade: string = EGrade.JUNIOR): number {
+    const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
+    return this._data[gradeKey]?.length;
+  }
 
-    public getQuestionAnswer(grade: string = EGrade.JUNIOR, questionId: number): string {
-        const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
-        const question = this._data[gradeKey].find(quest => quest.id === questionId)!;
-        if (this.isAnswerQuestionType(question)) {
-            return question.answer;
-        } else if (this.isClickQuestionType(question)) {
-            return question.options.find(option => option.isCorrect)!.text;
-        } else {
-            throw new Error('❌ No questions found for grade: ' + grade);
-        }
+  public getQuestionAnswer(
+    grade: string = EGrade.JUNIOR,
+    questionId: number
+  ): string {
+    const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
+    const question = this._data[gradeKey].find(
+      (quest) => quest.id === questionId
+    )!;
+    if (this.isAnswerQuestionType(question)) {
+      return question.answer;
+    } else if (this.isClickQuestionType(question)) {
+      return question.options.find((option) => option.isCorrect)!.text;
+    } else {
+      throw new Error("❌ No questions found for grade: " + grade);
     }
+  }
 
-    public getQuestionById(grade: string = EGrade.JUNIOR, id: number): TQuestion {
-        const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
-        return this._data[gradeKey][id];
-    }
+  public getQuestionById(grade: string = EGrade.JUNIOR, id: number): TQuestion {
+    const gradeKey = grade.toLowerCase() as keyof TQuestionsByGrade;
+    return this._data[gradeKey][id];
+  }
 }
--- source/types/logger.ts
+++ source/types/logger.ts
@@ -1,15 +1,14 @@
 export enum LogLevel {
-    ERROR = "error",
-    INFO = "info",
-    WARNING = "warning",
-    DEBUG = "debug"
+  ERROR = "error",
+  INFO = "info",
+  WARNING = "warning",
+  DEBUG = "debug",
 }
 
 export interface BotLogContext {
-    userId?: number,
-    chatId?: number,
-    messageId?: number,
-    questionId?: number,
-    grade?: string,
-    [key: string]: any
+  userId?: number;
+  chatId?: number;
+  messageId?: number;
+  questionId?: number;
+  grade?: string;
 }
